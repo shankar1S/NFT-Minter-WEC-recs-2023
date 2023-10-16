3672d6b714d96671bab9d864d4f8134f4d9ea43a
shankar1S <lvg.shankaragouda@gmail.com>
2023-10-16 13:48:16 +0530

    initial commit

---
+++ scripts/mint-nft.js
@@ -0,0 +1,40 @@
+
+require('dotenv').config();
+const ethers = require('ethers');
+
+// Get Alchemy API Key
+const API_KEY = process.env.API_KEY;
+
+// Define an Alchemy Provider
+const provider = new ethers.AlchemyProvider('sepolia', API_KEY)
+
+const contract = require("../artifacts/contracts/MyNFT.sol/MyNFT.json");
+
+// Create a signer
+const privateKey = process.env.PRIVATE_KEY
+const signer = new ethers.Wallet(privateKey, provider)
+
+// Get contract ABI and address
+const abi = contract.abi
+const contractAddress = '0xCBf7Fc670b07cc4bC97cf8D0E188F1b06df8Fb71'
+
+// Create a contract instance
+const myNftContract = new ethers.Contract(contractAddress, abi, signer)
+
+
+// Get the NFT Metadata IPFS URL
+const tokenUri = "https://gateway.pinata.cloud/ipfs/QmYueiuRNmL4MiA2GwtVMm6ZagknXnSpQnB3z2gWbz36hP"
+
+// Call mintNFT function
+const mintNFT = async () => {
+    let nftTxn = await myNftContract.mintNFT(signer.address, tokenUri)
+    await nftTxn.wait()
+    console.log(`NFT Minted! Check it out at: https://sepolia.etherscan.io/tx/${nftTxn.hash}`)
+}
+
+mintNFT()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
